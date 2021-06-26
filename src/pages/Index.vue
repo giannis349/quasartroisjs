ade711b4efaaf04ce4890204a5e5df072bec69c7
lm <luismguzmanz@gmail.com>
2021-06-25 19:19:46 -0500

    Instalar TroisJs e integrar componente .Vue al Layout

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,8 @@
 <template>
   <q-page class="flex flex-center">
-    <div class="main">
+    <div class="main fixed-top" style="width:100%; height:100vh;">
+
+      <Espacio3D/>
 
     </div>
   </q-page>
@@ -8,8 +10,13 @@
 
 <script>
 import { defineComponent } from 'vue'
+import Espacio3D from 'components/Espacio3D.vue'
 
 export default defineComponent({
-  name: 'PageIndex'
+  name: 'PageIndex',
+
+  components: {
+    Espacio3D
+  }
 })
 </script>
