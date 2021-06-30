e6d343fdb05e134faa922b84825478e8ecdc65fc
lm <luismguzmanz@gmail.com>
2021-06-30 08:09:11 -0500

    Ajustar a hash no history; Ajustar contenido tentaitvo del dialog; Ajustar luces y modelos 3D

--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -1,6 +1,8 @@
 <template>
 
-  <q-btn label="Maximized" color="primary" @click="dialog = true" style="z-index: 5000;"/>
+  <q-btn label="Educacion digital" color="primary" @click="dialog = true" style="z-index: 5000;"/>
+  <q-btn label="Publicaciones digitales" color="primary" @click="dialog = true" style="z-index: 5000;"/>
+  <q-btn label="Publicaciones digitales" color="primary" @click="dialog = true" style="z-index: 5000;"/>
 
   <q-dialog
     v-model="dialog"
@@ -11,31 +13,34 @@
   >
     <q-card class="bg-grey-1 text-gray-10">
 
-      <q-card-section style="max-width: 720px; margin: 0 auto;">
+      <q-card-section>
 
         <q-btn flat icon="close" v-close-popup>
           <q-tooltip class="bg-white text-primary">Cerrar</q-tooltip>
         </q-btn>
+        <section>
+          <div class="row justify-center">
+            <div class="col-xs-12 col-sm-10 col-md-5 col-lg-4">
+              <div class="text-h4 text-weight-bold text-gray-14 q-py-xl">Revistas digitales y publicaciones</div>
 
-        <div class="row">
-          <div class="col-xs-auto col-sm-12 col-md-6 col-lg-8">
-            <div class="text-h4 text-weight-bold text-gray-14 q-py-xl">Revistas digitales y publicaciones</div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum repellendus sit voluptate voluptas eveniet porro. Rerum blanditiis perferendis totam, ea at omnis vel numquam exercitationem aut, natus minima, porro labore.
+              </p>
 
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum repellendus sit voluptate voluptas eveniet porro. Rerum blanditiis perferendis totam, ea at omnis vel numquam exercitationem aut, natus minima, porro labore.
-            </p>
+              <q-btn class="bg-primary text-white q-my-lg">Ver publicaciones</q-btn>
+            </div>
+            <div class="col-xs-12 col-sm-10 col-md-5 col-lg-3 offset-lg-1 q-py-xl">
+              <img class="q-img main-images" src="~assets/dialog_img/global_temp_01.jpg" alt="globe">
+            </div>
           </div>
-          <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4">
-            <img class="q-img main-images" src="https://mux.com/static/masthead-screenshot-3eca6b08f9805ac28da65083039945e3.png" alt="Axios">
+          <div class="row justify-center dot-grid q-my-xl">
+            <div class="col-xs-12 col-sm-10 col-md-4 col-lg-4">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae corporis culpa cum cupiditate dolorem enim eveniet facere ipsum, libero minus obcaecati praesentium reiciendis rerum sapiente tempore. Consequatur in incidunt vero!
+              </p>
+            </div>
           </div>
-        </div>
-        <div class="row">
-          <div class="col-6">
-            <p class="dot-grid">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae corporis culpa cum cupiditate dolorem enim eveniet facere ipsum, libero minus obcaecati praesentium reiciendis rerum sapiente tempore. Consequatur in incidunt vero!
-            </p>
-          </div>
-        </div>
+        </section>
       </q-card-section>
     </q-card>
   </q-dialog>
@@ -59,7 +64,7 @@ export default {
   .main-images {
     border: 1px solid rgb(232, 232, 232);
     border-radius: 5px;
-    box-shadow: rgb(0 0 1.5) 0 10px 80px;
+    box-shadow: #cecece 0 0 20px;
   }
 
   .dot-grid {
--- src/components/Espacio3D.vue
+++ src/components/Espacio3D.vue
@@ -1,25 +1,29 @@
 <template>
   <Renderer ref="renderer" resize="window" :pointer="{ onMove: updateTilt }"
-            :orbit-ctrl="{ enableDamping: true, dampingFactor: 0.05, autoRotate: true, maxPolarAngle: Math.PI / 2,
+            :orbit-ctrl="{ enableDamping: true, dampingFactor: 0.005, autoRotate: true, maxPolarAngle: Math.PI / 2,
             screenSpacePanning: false, minDistance: 100, maxDistance: 2000 }"
             shadow>
     <Camera :position="{ x: 400, y: 200, z: 0 }"/>
-    <Scene ref="scene" background="#666666" :fog="['#ff0000', 0.9]">
-      <AmbientLight />
-      <PointLight ref="light" :position="{ y: 0, z: 20 }" />
-      <SpotLight color="#ffffff" :intensity="0.5" :position="{ y: 150, z: 0 }" :cast-shadow="true" :shadow-map-size="{ width: 1024, height: 1024 }" />
-      <SpotLight color="#4455ee" :intensity="0.5" :position="{ y: -150, z: 0 }" :cast-shadow="true" :shadow-map-size="{ width: 1024, height: 1024 }" />
-      <GltfModel @load="printe('loadeee')" :position="{x: 0, y: 0}" ref="teatro" src="3dmodels/teatro_jrr.gltf" :cast-shadow="true" :receive-shadow="true" />
-      <GltfModel :materialParams="{color: '#ffff00'}" :position="{x: 600, y: 0, z: 600}" src="3dmodels/teatro_jrr.gltf" :cast-shadow="true" :receive-shadow="true"></GltfModel>
-      <GltfModel :materialParams="{color: '#ffff00'}" :position="{x: 700, y: 0, z: 500}" src="3dmodels/teatro_jrr.gltf" :cast-shadow="true" :receive-shadow="true"></GltfModel>
-      <GltfModel :materialParams="{color: '#ffff00'}" :position="{x: 900, y: 0, z: 400}" src="3dmodels/teatro_jrr.gltf" :cast-shadow="true" :receive-shadow="true"></GltfModel>
-      <GltfModel :materialParams="{color: '#ffff00'}" :position="{x: 300, y: 0, z: 100}" src="3dmodels/teatro_jrr.gltf" :cast-shadow="true" :receive-shadow="true"></GltfModel>
+    <Scene ref="scene" background="#efffff
+" :fog=true>
+      <DirectionalLight color="#ceefff" :intensity="0.0" :position="{ x:2, y: 2, z: 0 }"/>
+      <PointLight ref="light" color="#ceefff" :intensity="4.0" :position="{ x:-200, y: 200, z: -500 }" />
+      <PointLight ref="light" color="#333333" :intensity="7.0" :position="{ x:200, y: 200, z: 200 }" />
+      <PointLight ref="light" color="#efffff" :intensity="3.5" :position="{ x:0, y: 500, z: 100 }" />
+      <SpotLight color="#ffffff" :intensity="0.0" :position="{ y: 150, z: -220 }" :cast-shadow="true" :shadow-map-size="{ width: 1024, height: 1024 }" />
+      <SpotLight color="#4455ee" :intensity="0.0" :position="{ y: -150, z: -250 }" :cast-shadow="true" :shadow-map-size="{ width: 1024, height: 1024 }" />
+
+      <GltfModel @load="printe('loadeee')"        :position="{x:   0, y: 0, z: 0}" ref="teatro" src="3dmodels/teatro_jrr.gltf" :cast-shadow="true" :receive-shadow="true" />
+      <GltfModel :materialParams="{color: '#ffff00'}" :position="{x: 200, y: 0, z: -250}" src="3dmodels/teatro_jrr.gltf" :cast-shadow="true" :receive-shadow="true"></GltfModel>
+      <GltfModel :materialParams="{color: '#ffff00'}" :position="{x: -200, y: 0, z:  300}" src="3dmodels/teatro_jrr.gltf" :cast-shadow="true" :receive-shadow="true"></GltfModel>
+      <GltfModel :materialParams="{color: '#ffff00'}" :position="{x: -250, y: 0, z:  -250}" src="3dmodels/teatro_jrr.gltf" :cast-shadow="true" :receive-shadow="true"></GltfModel>
+      <GltfModel :materialParams="{color: '#ffff00'}" :position="{x: 200, y: 0, z: -250}" src="3dmodels/teatro_jrr.gltf" :cast-shadow="true" :receive-shadow="true"></GltfModel>
     </Scene>
     <EffectComposer>
       <RenderPass/>
-      <UnrealBloomPass :strength="0.3"/>
+      <UnrealBloomPass :strength="0.05"/>
       <FXAAPass />
-      <TiltShiftPass :gradient-radius="tiltRadius" :start="{ x: 0, y: this.tiltY }" :end="{ x: 100, y: this.tiltY }" />
+      <TiltShiftPass :gradient-radius="tiltRadius" :start="{ x: 0, y: this.tiltY }" :end="{ x: 0, y: this.tiltY }" />
     </EffectComposer>
   </Renderer>
 </template>
@@ -32,6 +36,8 @@ import {
   StandardMaterial,
   Camera,
   AmbientLight,
+  DirectionalLight,
+  PointLight,
   GltfModel,
   EffectComposer,
   Renderer,
@@ -52,6 +58,8 @@ export default {
     Camera,
     GltfModel,
     AmbientLight,
+    DirectionalLight,
+    PointLight,
     EffectComposer,
     Renderer,
     RenderPass,
@@ -68,8 +76,8 @@ export default {
   },
   data () {
     return {
-      tiltRadius: 100,
-      tiltY: 100
+      tiltRadius: 520,
+      tiltY: 520
     }
   },
   methods: {
@@ -78,6 +86,7 @@ export default {
     },
     updateTilt ({ positionN }) {
       this.tiltRadius = this.size.height / 3
+      this.tiltY = this.size.height / 2;
       this.tiltY = (positionN.y + 1) * 0.5 * this.size.height
     }
   },
